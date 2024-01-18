5c6ef0748437b8185871e2d16bc9b05d4841b6c5
MinhDucMa <77559009+MinhDucMa@users.noreply.github.com>
2024-01-17 23:31:07 -0800

    Update animation for popup form

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function openModal() {
   });
 }
 
-function closeModal() {
+function closeModalButton() {
   const closeButton = document.querySelector(".close-button");
   const formModal = document.querySelector("#popupForm");
   const allInputFields = document.querySelectorAll(".input-field");
@@ -16,4 +16,27 @@ function closeModal() {
   });
 }
 
+function submitFormButton() {
+  const submitButton = document.querySelector(".submit-button");
+  const formModal = document.querySelector("#popupForm");
+  const allInputFields = document.querySelectorAll(".input-field");
+  submitButton.addEventListener("click", () => {
+    allInputFields.forEach((singleInputField) => (singleInputField.value = ""));
+    formModal.close();
+  });
+}
+
+function getFormData() {
+  const form = document.querySelector("form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const formData = new FormData(form);
+
+    for (item of formData) {
+      console.log(item);
+    }
+  });
+}
+
 openModal();
+submitFormButton();
