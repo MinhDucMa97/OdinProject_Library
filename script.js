d632a2c7c727da45c93b741b94711d35c0a08f31
MinhDucMa <77559009+MinhDucMa@users.noreply.github.com>
2024-02-06 00:52:43 -0800

    Update: Implement Add and Delete Book Feature on Indiviual Card

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let booksArray = [];
+let currentIndex = 0;
+
 function openModal() {
   const openButton = document.querySelector(".add-button");
   const formModal = document.querySelector("#popupForm");
@@ -16,27 +19,94 @@ function closeModalButton() {
   });
 }
 
-function submitFormButton() {
-  const submitButton = document.querySelector(".submit-button");
+// function submitFormButton() {
+//   const submitButton = document.querySelector(".submit-button");
+//   const formModal = document.querySelector("#popupForm");
+//   const allInputFields = document.querySelectorAll(".input-field");
+//   submitButton.addEventListener("click", () => {
+//     allInputFields.forEach((singleInputField) => (singleInputField.value = ""));
+//     formModal.close();
+//   });
+// }
+
+function submitForm(event) {
+  event.preventDefault();
+
+  const title = document.getElementById("book-title").value;
+  const author = document.getElementById("book-author").value;
+  const pages = document.getElementById("book-pages").value;
+  const readStatus = document.getElementById("book-status");
   const formModal = document.querySelector("#popupForm");
   const allInputFields = document.querySelectorAll(".input-field");
-  submitButton.addEventListener("click", () => {
-    allInputFields.forEach((singleInputField) => (singleInputField.value = ""));
-    formModal.close();
-  });
+
+  let bookStatus = readStatus.checked ? "Read" : "Unread";
+
+  const newBookObject = {
+    index: currentIndex,
+    title: title,
+    author: author,
+    pages: pages,
+    bookStatus: bookStatus,
+  };
+
+  currentIndex++;
+
+  allInputFields.forEach((singleInputField) => (singleInputField.value = ""));
+  formModal.close();
+
+  booksArray.push(newBookObject);
+
+  displayBooks();
 }
 
-function getFormData() {
-  const form = document.querySelector("form");
-  form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    const formData = new FormData(form);
+function deleteBook(index) {
+  booksArray = booksArray.filter((book) => {
+    return book.index !== index;
+  });
 
-    for (item of formData) {
-      console.log(item);
-    }
+  displayBooks();
+}
+
+function displayBooks() {
+  let cardContainer = document.querySelector(".card-container");
+
+  cardContainer.innerHTML = "";
+
+  booksArray.forEach((book) => {
+    let card = document.createElement("div");
+    card.className = "card-element";
+
+    let bookTitle = document.createElement("div");
+    bookTitle.className = "book-title";
+    bookTitle.textContent = book.title;
+
+    let bookAuthor = document.createElement("div");
+    bookAuthor.className = "book-author";
+    bookAuthor.textContent = book.author;
+
+    let bookPages = document.createElement("div");
+    bookPages.className = "book-pages";
+    bookPages.textContent = book.pages;
+
+    let bookStatus = document.createElement("div");
+    bookStatus.className = "book-status";
+    bookStatus.textContent = book.bookStatus;
+
+    let singleBookRemoveBtn = document.createElement("button");
+    singleBookRemoveBtn.className = "remove-btn";
+    singleBookRemoveBtn.textContent = "Delete";
+    singleBookRemoveBtn.addEventListener("click", () => {
+      deleteBook(book.index);
+    });
+
+    card.appendChild(bookTitle);
+    card.appendChild(bookAuthor);
+    card.appendChild(bookPages);
+    card.appendChild(bookStatus);
+    card.appendChild(singleBookRemoveBtn);
+
+    cardContainer.appendChild(card);
   });
 }
 
 openModal();
-submitFormButton();
